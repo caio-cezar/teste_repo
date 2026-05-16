147ec485052aad854c24c8740968649547a8ef83
Caio Cezar <62731106+caio-cezar@users.noreply.github.com>
2026-05-15 23:04:11 -0300

    Update primeiro.py

--- primeiro.py
+++ primeiro.py
@@ -1,13 +1,3 @@
 import os
-import psycopg
-from dotenv import load_dotenv
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-print("DATABASE_URL =", DATABASE_URL)
-
-conn = psycopg.connect(DATABASE_URL)
-
-print("Conectado")
+print(os.getenv("DATABASE_URL"))
