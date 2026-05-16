ffa1c4187d5c28b694bb8a2c4a07c7fe4c266611
Caio Cezar <62731106+caio-cezar@users.noreply.github.com>
2026-05-15 23:02:17 -0300

    Update primeiro.py

--- primeiro.py
+++ primeiro.py
@@ -6,21 +6,8 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+print("DATABASE_URL =", DATABASE_URL)
+
 conn = psycopg.connect(DATABASE_URL)
 
-cur = conn.cursor()
-
-cur.execute(
-    """
-    INSERT INTO public.clientes (cpf, nome, pais)
-    VALUES (%s, %s, %s)
-    """,
-    ("12312312332", "Caio Pedro", "Portugal")
-)
-
-conn.commit()
-
-print("Cliente inserido")
-
-cur.close()
-conn.close()
+print("Conectado")
