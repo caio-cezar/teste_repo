eb90a9ad20169c817c2165bacab2a5e09108c5c0
Caio Cezar <62731106+caio-cezar@users.noreply.github.com>
2026-05-15 22:55:28 -0300

    Update primeiro.py

--- primeiro.py
+++ primeiro.py
@@ -1,7 +1,26 @@
-import pandas as pd
-import requests
+import os
+import psycopg
+from dotenv import load_dotenv
 
-x = requests.get('https://api.maisretorno.com/v3/general/quotes/ihfa:idx?adjusted=true').json()
+load_dotenv()
 
-print('Requisicao realizada com sucesso.')
-print(x)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+conn = psycopg.connect(DATABASE_URL)
+
+cur = conn.cursor()
+
+cur.execute(
+    """
+    INSERT INTO public.clientes (cpf, nome, pais)
+    VALUES (%s, %s, %s)
+    """,
+    ("12312312332", "Caio Pedro", "Portugal")
+)
+
+conn.commit()
+
+print("Cliente inserido")
+
+cur.close()
+conn.close()
